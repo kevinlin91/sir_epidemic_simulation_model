0502e89af0880c9041a18cfc65d68e327334a8fd
kevin <kevin99703012@gmail.com>
2018-07-28 14:34:27 +0800

    update

---
+++ model.py
@@ -0,0 +1,165 @@
+from util import *
+import os
+import pickle
+import random
+import queue
+from copy import deepcopy
+
+class SIR_model():
+
+    def __init__(self):
+        self.population = self.loading_population()
+        self.lining_neighbor, self.lining_list = self.loading_lining()
+        self.G = self.loading_graph()
+        self.mos = self.loading_mos()
+
+    
+
+    def loading_lining(self):
+        if (os.path.isfile('./lining_list.pkl') and os.path.isfile('./lining_neighbor.pkl')):
+            return pickle.load(open('./lining_neighbor.pkl','rb')), \
+                   pickle.load(open('./lining_list.pkl','rb'))
+        else:
+            lining_neighbor = dict()
+            with open('./neighbor.txt',encoding='utf8') as f:
+                for line in f.readlines():
+                    line = line.strip().split(',')
+                    key = line[0].replace(u'\ufeff', '')
+                    value = line[1:]
+                    lining_neighbor[key] = value
+            lining_list = list(lining_neighbor.keys())
+            pickle.dump(lining_neighbor, open('./lining_neighbor.pkl','wb'))
+            pickle.dump(lining_list, open('./lining_list.pkl','wb'))
+            return lining_neighbor, lining_list                            
+    def loading_population(self):
+        if (os.path.isfile('./population.pkl')):
+            return pickle.load(open('./population.pkl','rb'))
+        else:
+            population = population_filter('北區')
+            pickle.dump(population, open('./population.pkl','wb'))
+            return population
+    def loading_graph(self):
+        if (os.path.isfile('./graph.pkl')):
+            return pickle.load(open('./graph.pkl','rb'))
+        else:
+            G = create_graph(self.population, self.lining_list, self.lining_neighbor)
+            pickle.dump(G, open('./graph.pkl','wb'))
+            return G
+    def loading_mos(self):
+        mos_dict = dict()
+        for lining in self.lining_list:
+            date_dict = dict()
+            for i in range(365):
+                date_dict[i] = random.randint(100,300)
+            mos_dict[lining] = date_dict
+        return mos_dict
+
+    
+    def simulation(self):
+        first_day_mos_rate = 0.1
+        effect_rate = 0.6
+        queue_dict = dict()
+        for day in range(30):
+            if day == 0:
+                for lining in self.lining_list:
+                    #mos_information_update
+                    total_mos = self.mos[lining][day]
+                    ill_mos = int(total_mos * first_day_mos_rate)
+                    health_mos = total_mos - ill_mos
+                    queue_tmp = queue.Queue()
+                    queue_tmp.put(ill_mos)
+                    self.G.nodes[lining]['health_mos'] = health_mos
+                    self.G.nodes[lining]['ill_mos'] = ill_mos
+                    queue_dict[lining] = queue_tmp
+                    #mos_contact_update
+                    neighbor = [x for x in self.G.neighbors(lining)]
+                    effect_people = list()
+                    for mor_index in range(ill_mos):
+                        selection = random.choices(neighbor, k=3)
+                        effect_people += selection
+                    effect_people = list(set(effect_people))
+                    for people in effect_people:
+                        if random.random() < 0.6:
+                            self.G.nodes[people]['status'] = 'E_1'
+            else:
+                #status update
+                for node in self.G:
+                    if node not in self.lining_list:
+                        status_str = self.G.nodes[node]['status'].split('_')
+                        status = status_str[0]                        
+                        delay = self.G.nodes[node]['delay']
+                        #E update
+                        if status == 'E':
+                            status_day = int(status_str[1])
+                            if status_day == delay:
+                                if random.random() < 0.4669:
+                                    self.G.nodes[node]['status'] = 'I_1'
+                                else:
+                                    self.G.nodes[node]['status'] = 'R'
+                            else:
+                                self.G.nodes[node]['status'] = 'E_' + str(status_day+1)
+                        elif status == 'I':
+                            status_day = int(status_str[1])
+                            if status_day == 7:
+                                self.G.nodes[node]['status'] = 'R'
+                            else:
+                                self.G.nodes[node]['status'] = 'I_' + str(status_day+1)
+                            
+                for lining in self.lining_list:                    
+                    #mos_information_update
+                    total_mos = self.mos[lining][day]
+                    ill_mos = self.G.nodes[lining]['ill_mos']
+                    health_mos = total_mos - ill_mos
+                    if day > 20:
+                        ill_mos = ill_mos - queue_dict[lining].get()
+                    #mos_contact_update
+                    neighbor = [x for x in self.G.neighbors(lining)]
+                    effect_people = list()
+                    for mor_index in range(ill_mos):
+                        selection = random.choices(neighbor, k=3)
+                        effect_people += selection
+                    effect_people = list(set(effect_people))
+                    for people in effect_people:
+                        if self.G.nodes[people]['status'] == 'S':
+                            if random.random() < 0.6: 
+                                self.G.nodes[people]['status'] = 'E_1'
+                    #mos_ill_by_people
+                    count = 0
+                    for mor_index in range(health_mos):
+                        selection = random.choices(neighbor, k=3)
+                        for nei in neighbor:
+                            if self.G.nodes[nei]['status'].split('_')[0] == 'I':
+                                count +=1                    
+                    #mos_information_update_after_contact
+                    ill_mos += count
+                    health_mos = total_mos - ill_mos
+                    self.G.nodes[lining]['health_mos'] = health_mos
+                    self.G.nodes[lining]['ill_mos'] = ill_mos                    
+                    queue_dict[lining].put(count)
+            E_count = 0
+            I_count = 0
+            R_count = 0
+            for node in self.G:
+                if node not in self.lining_list:
+                    status_str = self.G.nodes[node]['status'].split('_')
+                    status = status_str[0]
+                    if status == 'E':
+                        E_count +=1
+                    elif status == 'I':
+                        I_count +=1
+                    elif status == 'R':
+                        R_count +=1
+            print ('Day:', day, ' E:', E_count,' I:', I_count, ' R:', R_count)
+                
+                
+                                
+                                
+                            
+                
+                    
+
+
+if __name__ == '__main__':
+    model = SIR_model()
+    model.simulation()
+            
---
+++ util.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import pickle
+import networkx as nx
+import random
+
+def connect(age):
+    if (age == '0歲' or age == '1歲' or age == '2歲' or age == '3歲' or \
+       age == '4歲' or age == '5歲-9歲' or age == '65歲-69-歲' or \
+       age =='70歲以上' ):
+        return False
+    else:
+        return True
+
+
+def population_filter(area):
+    population = pd.read_csv('./file_188Ｗ.csv', engine='python', encoding='utf8', delimiter=',')
+    
+    population.columns = ['index', 'sex', 'total_area', 'age']
+    population.set_index('index', inplace=True)
+    population['first_area'] = population['total_area'].str.rpartition('-')[0]
+    population['second_area'] = population['total_area'].str.rpartition('-')[2]
+    
+    return (population[ population['first_area']==area])
+
+def create_graph(population, lining_list, lining_neighbor):
+    population = population.reset_index(drop=True)
+    G = nx.Graph()
+    G.add_nodes_from(lining_list, ill_mos = 0, health_mos = 0)
+    for index, row in population.iterrows():
+        G.add_node(str(index), status = 'S', delay = random.randint(4,8))
+        G.add_edge(str(index), row['second_area'])
+        if connect(row['age']):
+            if random.random() > 0.7:
+                source_area = row['second_area']
+                neighbor = lining_neighbor[source_area]
+                G.add_edge(str(index), random.choice(neighbor))
+    return G
+    #print (G.number_of_edges())
+    #print (list(G.neighbors('1')))
+    #print (G.nodes['1'])
+    #print (G.nodes['嘉芳里'])
+
+
+if __name__ == '__main__':
+    #population = population_filter('新營區')
+    create_graph(pickle.load(open('./population.pkl','rb')), \
+                 pickle.load(open('./lining_list.pkl','rb')), \
+                 pickle.load(open('./lining_neighbor.pkl','rb')))
